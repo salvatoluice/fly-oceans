71b1d7bed6b2716ccfa7cd21a38b8a2a596caea7
Salvato Luice <salvatoluice5@gmai.com>
2023-01-03 21:38:58 +0300

    Add commits

--- src/components/mainPage/Sdata.js
+++ src/components/mainPage/Sdata.js
@@ -1,27 +1,27 @@
 const Sdata = [
     {
       id: 1,
-      title: "50% Off For Your First Shopping",
+      title: "Fly-Oceans!, a new shopping solution!",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-      cover: "./images/SlideCard/slide-1.png",
+      cover: "https://images.unsplash.com/photo-1671725779305-bff966b40cbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
     },
     {
       id: 2,
-      title: "50% Off For Your First Shopping",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-      cover: "./images/SlideCard/slide-2.png",
+      title: "Worldwide Shipping, International markets covered, Enjoy!",
+      desc: "Fly-Oceans is here to satisfy all your needs, Quality of Service is our assigned. Shipping done worldwide within one week after order placement.",
+      cover: "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
     },
     {
       id: 3,
-      title: "50% Off For Your First Shopping",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-      cover: "./images/SlideCard/slide-3.png",
+      title: "Fly-Oceans,across the borders!",
+      desc: "Instant deliveries, payment upon delivery, we value your time shopping with us, browse althrough to see all discounts, we got you covered.",
+      cover: "https://images.unsplash.com/photo-1671725501844-1e6d0081bf64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw1OXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
     },
     {
       id: 4,
       title: "50% Off For Your First Shopping",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-      cover: "./images/SlideCard/slide-4.png",
+      cover: "https://images.unsplash.com/photo-1661961112835-ca6f5811d2af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
     },
   ]
   export default Sdata
--- src/components/mainPage/SlideCard.js
+++ src/components/mainPage/SlideCard.js
@@ -25,7 +25,7 @@ const SlideCard = () => {
                 <div className='left'>
                   <h1>{value.title}</h1>
                   <p>{value.desc}</p>
-                  <button className='btn-primary'>Visit Collections</button>
+                  <button className='btn-primary'>See Collections</button>
                 </div>
                 <div className='right'>
                   <img src={value.cover} alt='' />
