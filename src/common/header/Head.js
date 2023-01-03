458ceac3a59368754a81301e5eb5d6b810017a09
Salvato Luice <salvatoluice5@gmai.com>
2023-01-03 12:45:55 +0300

    Add header

---
+++ src/common/header/Head.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +254 113 794 219</label>
+            <i className='fa fa-envelope'></i>
+            <label>fly-oceans@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>FAQ"s</label>
+            <label>Get Help</label>
+            <span>🏳️‍⚧️</span>
+            <label>EN</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
